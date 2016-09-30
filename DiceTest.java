4e896d755ec45ce5a389c34a06ca125b310407a3
jeppe89 <keke3k2@gmail.com>
2016-09-30 09:39:52 +0200

    Oprettet ny test af roll()

--- DiceTest.java
+++ DiceTest.java
@@ -7,18 +7,25 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class DiceTest {
-
+	Dice dice;
 	@Before
 	public void setUp() throws Exception {
+		dice = new Dice();
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		dice = null;
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testRoll() {
+		int value;
+		for(int i = 0; i< 100; i++)
+		{
+			value = dice.roll();
+			if(value < 1 || value > 6)
+				fail("Terning fik værdi mindre end 1 eller større end 6");
+		}
 	}
-
 }
